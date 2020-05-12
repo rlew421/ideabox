f3ebe0724ff54e683e7b929d57afd0aaf17ef2a7
Rachel Lew <rlew07@gmail.com>
2020-05-11 19:28:58 -0600

    Add conditional rendering

--- src/App.js
+++ src/App.js
@@ -23,8 +23,9 @@ class App extends Component {
 
   render() {
     return (
-      <main>
+      <main className='App'>
         <h1>Ideabox</h1>
+        {!this.state.ideas.length && <h2>No ideas yet -- add some!</h2>}
         <Form addIdea={this.addIdea}/>
         <Ideas ideas= {this.state.ideas}/>
       </main>
